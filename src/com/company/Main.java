6da5868739a300d9dd98cd6784ec4e8976c8e5ee
Platon1720 <vanokonovalenko123123@gmail.com>
2020-08-20 16:42:16 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+         int a = 12;
+
+        System.out.println(a == 1 ? "а равно 1" : "a не равно 1");
+
+    }
+}
